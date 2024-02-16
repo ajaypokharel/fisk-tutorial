a7f40a6a50a095a8cc3e08f1e9631da9fa2512a7
Ajay Pokharel <ajaypokharel8@gmail.com>
2024-02-16 17:56:29 -0600

    Adds ai generated code

--- xyz.py
+++ xyz.py
@@ -1,1 +1,9 @@
-# hello
+
+def num(x):
+    """ x ** 2
+    Returns the square of the given number.
+    Parameters:
+        - x (int or float): Number to be squared.
+    Returns:
+        - int or float: Square of the given number."""
+    return x**2
